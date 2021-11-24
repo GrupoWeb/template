e32e97f5657834ba454f78bc45a99b448a5b61e6
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2021-11-24 11:57:40 -0600

    link

--- laravel/coreui/src/main.js
+++ laravel/coreui/src/main.js
@@ -13,7 +13,8 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 
 Vue.use(VueSweetalert2)
-Vue.prototype.$apiAdress = 'http://127.0.0.1:8000'
+// Vue.prototype.$apiAdress = 'http://127.0.0.1:8000'
+Vue.prototype.$apiAdress = 'https://dev.diaco.gob.gt/'
 Vue.config.performance = true
 Vue.use(CoreuiVue)
 
